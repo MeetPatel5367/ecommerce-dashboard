6035a8e01d3b5a8a647b27333493ca808a778523
MeetPatel5367 <meetpatel5367@gmail.com>
2026-04-06 02:45:38 -1200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+placeholder = st.empty()
 
 st.set_page_config(
     page_title="E-commerce Intelligence Dashboard",
